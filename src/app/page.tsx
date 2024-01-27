cfb6efec33e20f88a1255b444809ab331a5d9237
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-01-28 01:53:28 +0530

    feat: setup material ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
 /**
+ * © IEEE CS Kerala Chapter 2024. All rights reserved.
  *
- * This code is the property of Code-Theft_ and is protected by copyright law.
+ * This code is the property of IEEE CS Kerala Chapter and is protected by copyright law.
  * Unauthorized use, reproduction, or distribution is strictly prohibited.
- *
+ * 
  * @author Janajith D
  */
 
 import React from "react";
 import Link from "@components/Link";
+import Button from "@components/Button";
+import HomeIcon from "@icons/Home";
 
 export default function Home() {
   return (
     <>
       <p>Home Page</p>
-      <Link href="new page">Hello</Link>
+      <HomeIcon/><Link href="new page">Hello</Link>
+      <Button variant="contained">Hello NextJS</Button>
+
     </>
   );
 }
